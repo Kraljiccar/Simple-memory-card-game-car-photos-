581c0ec04cf9560569e5f0d8317b42c83786a777
Kraljiccar <nemanjakraljic@gmail.com>
2020-05-18 17:26:52 +0000

    random

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+var source = "#game";
+
+var Imgsource = [
+    "assets/images/fiat500.jpg",
+    "assets/images/beetle.jpg",
+    "assets/images/ferrrari_f40.jpg",
+    "assets/images/ford_gt.jpg",
+    "assets/images/GTO.jpg",
+    "assets/images/mazdarx9.jpg",
+    "assets/images/porsche_911.jpg",
+    "assets/images/subaru_impreza.jpg",
+    "assets/images/tesla.jpg",
+    "assets/images/odltimer.jpg",
+];
+
+function RandomFunction(MaxValue, MinValue) {
+		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+	}
